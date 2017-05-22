c03d33db4565cd05329c39349c73be6b9a89b4bc
Charles Mihran <cmihran@cs.washington.edu>
2017-05-22 01:55:03 -0700

    made api call reflecive, must faster results now

--- server.R
+++ server.R
@@ -14,16 +14,16 @@ base.uri <- "http://pokeapi.co/api/v2/"
 QueryApi <- function(query) {
   full.uri <- paste0(base.uri, query)
   response <- GET(full.uri)
-  data <- fromJSON(content(response, "text", encoding = "UTF-8"))
-  return(data)
+  pokemon.df <- fromJSON(content(response, "text", encoding = "UTF-8"))
+  return(pokemon.df)
 }
 
 # Builds a dataframe representing a pokemons stats for use with ggplot
 # Takes in a dataset from a pokemon query to the API
-BuildStatRadar <- function(data) {
+BuildStatRadar <- function(pokemon.df) {
   # get the data we want from the data frame
-  stat.df <- data.frame(data$stats$stat$name, data$stats$base_stat)
-  stat.df <- stat.df %>% rename("Stat" = data.stats.stat.name, "Value" = data.stats.base_stat)
+  stat.df <- data.frame(pokemon.df$stats$stat$name, pokemon.df$stats$base_stat)
+  stat.df <- stat.df %>% rename("Stat" = pokemon.df.stats.stat.name, "Value" = pokemon.df.stats.base_stat)
   
   # organize the stats in a way that works for ggplot
   speed <- stat.df %>% filter(Stat == "speed")
@@ -32,7 +32,7 @@ BuildStatRadar <- function(data) {
   defense <- stat.df %>% filter(Stat == "defense")
   attack <- stat.df %>% filter(Stat == "attack")
   spa <- stat.df %>% filter(Stat == "special-attack")
-  Pokemon <- capitalize(data$name)
+  Pokemon <- capitalize(pokemon.df$name)
   stat.radar.df <- data.frame(Pokemon, "Special Defense" = spd$Value,"Speed" = speed$Value, "Health" = hp$Value,
                               "Special Attack" = spa$Value, "Attack" = attack$Value, "Defense" = defense$Value, check.names = FALSE)
   
@@ -50,40 +50,43 @@ capitalize <- function(word) {
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+  pokemon.df <- reactive({
+    pokemon.df <- QueryApi(paste0("pokemon/", input$pokemon))    
+  })
+  
   output$pokedata <- renderPrint({
-    data <- QueryApi(paste0("pokemon/", input$pokemon))
-    cat(paste0("ID: ", data$id,"\n"))
-    cat(paste0("Name: ", data$name,"\n"))
-    cat(paste0("Base Experience: ", data$base_experience,"\n"))
-    cat(paste0("Height: ", data$height,"\n"))
-    cat(paste0("Weight: ", data$weight,"\n"))
+    pokemon.df <- pokemon.df()
+    cat(paste0("ID: ", pokemon.df$id,"\n"))
+    cat(paste0("Name: ", pokemon.df$name,"\n"))
+    cat(paste0("Base XP: ", pokemon.df$base_experience,"\n"))
+    cat(paste0("Height: ", pokemon.df$height,"\n"))
+    cat(paste0("Weight: ", pokemon.df$weight,"\n"))
     cat(paste0("Type(s): "))
-    cat(data$types$type$name)
+    cat(pokemon.df$types$type$name)
   })
   
   output$plot <- renderPlotly({
-    data <- QueryApi(paste0("pokemon/", input$pokemon))
-    stat.df <- data.frame(data$stats$stat$name, data$stats$base_stat)
-    stat.df <- stat.df %>% rename("Stat" = data.stats.stat.name, "Value" = data.stats.base_stat)
+    pokemon.df <- pokemon.df()
+    stat.df <- data.frame(pokemon.df$stats$stat$name, pokemon.df$stats$base_stat)
+    stat.df <- stat.df %>% rename("Stat" = pokemon.df.stats.stat.name, "Value" = pokemon.df.stats.base_stat)
     
     plot_ly(type = "bar", data = stat.df, x = ~Stat, y = ~Value) %>% layout(yaxis = list(range = c(0, max.stat)))
   })
   
   output$radar <- renderPlot({
-    data <- QueryApi(paste0("pokemon/", input$pokemon))
-    stat.radar.df <- BuildStatRadar(data)
+    pokemon.df <- pokemon.df()
+    stat.radar.df <- BuildStatRadar(pokemon.df)
     ggradar(stat.radar.df)
   })
   
   output$image <- renderImage({
-    data <- QueryApi(paste0("pokemon/", input$pokemon))
-    
-    filename <- normalizePath(file.path('./www/assets/imgs/sprites/', paste0(data$id, '.png')))
+    pokemon.df <- pokemon.df()
+    filename <- normalizePath(file.path('./www/assets/imgs/sprites/', paste0(pokemon.df$id, '.png')))
     
     list(src = filename,
          width = 187,
          height = 187,
-         alt = paste("Sprite", data$id))
+         alt = paste("Sprite", pokemon.df$id))
     
   }, deleteFile = FALSE)
   
